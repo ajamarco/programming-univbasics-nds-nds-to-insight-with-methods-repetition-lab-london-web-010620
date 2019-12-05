12f5c7abacf0b74da7fa01b71653d5ac2e984bea
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-05 21:20:20 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -43,8 +43,7 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  directors = list_of_directors(source)
-  puts directors
+  
   
 end
 
